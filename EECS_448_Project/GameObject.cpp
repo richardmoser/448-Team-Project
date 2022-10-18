269058d34230cc45328e746f769719f72ef26993
Harrison Reed <harrison.reed16@gmail.com>
2022-10-17 23:46:47 -0500

    Added Texture Map functionality( This is under Map.cpp and Map.h). This allows for drawing the map the player is currently on. Also added The Entity Component System(This is under ECS.h and Components.h). This allows us to better manage the Entities in our system, i.e players/ai.V2

---
+++ EECS_448_Project/GameObject.cpp
@@ -0,0 +1,51 @@
+#include "GameObject.h"
+#include "TextureManager.h"
+#include <iostream>
+#include <math.h>
+
+GameObject::GameObject(const char* textureSheet, int x, int y) {
+	
+	objTexture = TextureManager::LoadTexture(textureSheet);
+
+	xpos = x;
+	ypos = y;
+	//srcRect.x = 0;
+	//srcRect.y = 0;
+	srcRect.h = 62;
+	srcRect.w = 50;
+	destRect.w = srcRect.w * 2;
+	destRect.h = srcRect.h * 2;
+	
+
+}
+
+void GameObject::Update() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	destRect.x = xpos;
+	destRect.y = ypos;
+	
+
+		switch (event.key.keysym.sym) {
+		case SDLK_d:
+				xpos += 100;
+				break;
+		case SDLK_a:
+				xpos -= 50;
+				break;
+		case SDLK_s:
+				ypos += 100;
+				break;
+		case SDLK_w:
+				ypos -= 100;
+				break;
+		default:
+			break;
+		}
+
+}
+
+void GameObject::Render() {
+	SDL_RenderCopy(game::renderer, objTexture, &srcRect, &destRect);
+}
---
+++ EECS_448_Project/Map.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Game.h"
+
+class Map {
+
+	public:
+
+		Map();
+		~Map();
+
+		void loadMap(int arr[20][25]);
+		void drawMap();
+
+	private:
+
+		SDL_Rect src, dest;
+
+		SDL_Texture* dirt;
+		SDL_Texture* grass;
+		SDL_Texture* water;
+
+		int map[20][25];
+
+};
